2c451ffae7c54c59081182404afda93674768a36
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-09-29 10:02:58 +0530

    add new code

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -1,5 +1,61 @@
-const tinderUser = new Object() // ,== singlton Object 
-console.log(tinderUser);
+// const tinderUser = new Object() // ,== singlton Object 
+// console.log(tinderUser);
 
-const theTinderUser = {} // <== non singlton Object
-console.log(theTinderUser)
+// const theTinderUser = {} // <== non singlton Object
+// console.log(theTinderUser)
+
+const tinderUser ={}
+tinderUser.id = "123abc",
+tinderUser.name = "some";
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+         userfullname: {
+            firstname: "Tanveer",
+            lastname: "Pathan"
+         }
+    },
+    gender: "Male"
+}
+// console.log(regularUser.fullname);
+// console.log(regularUser.gender);
+// console.log(regularUser.fullname?.userfullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+// const obj4 = {5: "a", 6: "b"}
+// const obj3 = {obj1, obj2};
+// const obj3 = Object.assign({},obj1,obj2,obj4);
+
+const obj3 = {...obj1, ...obj2} // use all thime spred operator
+// console.log(obj3)
+
+const users = [
+    {
+        id: 1,
+        email: "a@gmail.com",
+    },
+    {
+        id: 2,
+        email: "a@gmail.com",
+    },
+    {
+        id: 3,
+        email: "a@gmail.com",
+    },
+    {
+        id: 4,
+        email: "a@gmail.com",
+    },
+]
+users[1].email
+console.log(tinderUser)
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('isLogged'));
