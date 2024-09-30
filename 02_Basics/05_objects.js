aa7cf23e5d185112850fe620dd98bbe4ab2adeb5
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-09-30 11:15:21 +0530

    How to destructer Objects with code

--- 02_Basics/05_objects.js
+++ 02_Basics/05_objects.js
@@ -1,1 +1,13 @@
-const 
+// How to destructer Objects
+
+const course = {
+    coursename: "JS in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor 
+
+// destructer Objects
+const {courseInstructor: instructor, price} = course
+console.log(instructor);
