767eebda0340630cbfddad0c6e2bac07ccfc8183
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-10-09 08:50:35 +0530

    IIFE Immediately Invoked

---
+++ 03_Basics/05_IIFE.js
@@ -0,0 +1,27 @@
+// Immediately Invoked function ecpressions (IIFE)
+
+// IIFE ka matlab hai "Immediately Invoked Function Expression".
+// Yeh ek aisi JavaScript function hoti hai jo turant declare
+// karne ke baad execute ho jati hai, bina us function ko \
+// explicitly call karne ki zarurat.IIFE ko istara likha jata hai:
+
+// 1) Global Scope Polluting ko Avoid karna IIFE local scope create
+// karta hai jisme variables aur functions ko isolate kiya jata hai.
+// Isse global scope mein unnecessary variables ko introduce karne
+// se bach sakte hain.
+
+// 2) Encapsulation: Agar tum chahte ho ke tumhare function ke variables 
+// dusre code se interfere na ho sakein, to IIFE ka use karke tum unhe encapsulate kar sakte ho.
+
+// 3) Asynchronous Code ke liye: Asynchronous tasks jaha tum chahte 
+// ho ke koi code turant execute ho, IIFE helpful hota hai.
+
+(function chai() {
+    console.log(`DB CONNECTED`);
+})()
+
+// () --> Exicution
+// () --> funciton call
+
+// chai()
+
