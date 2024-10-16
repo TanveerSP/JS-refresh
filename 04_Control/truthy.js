bc943c3db65e6a880bd34849d15ceb86432a2704
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-10-16 16:13:40 +0530

    code and notes

--- 04_Control/truthy.js
+++ 04_Control/truthy.js
@@ -1,10 +1,28 @@
 // const userEmail = "t@nveer.ai"
 // const userEmail = ""
-// const userEmail = []
-const userEmail = 1
+const userEmail = []
+// const userEmail = 1
 
-if(userEmail) {
-    console.log("Got user email")
-} else{
-    console.log("Don't have user email")
+// if(userEmail) {
+//     console.log("Got user email")
+// } else{
+//     console.log("Don't have user email")
+// }
+
+// Truthy and Falsy value
+
+// // Truthy values
+// true, 1, [], {}, "0", 'false', " ", function(){},  etc..
+
+// // falsy values
+// false, 0, -0, BigInt 0n, "", '', null, undefind, NaN etc...
+
+// if(userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length === 0){
+    console.log("Object is empty")
 }
