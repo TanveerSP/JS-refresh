7f8e8d623df42e4ce8455c4089d2d9584e3e58b3
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-10-16 15:58:08 +0530

    code

---
+++ 04_Control/switch.js
@@ -0,0 +1,52 @@
+// ======================================
+// sntaxy for switch case 
+// ======================================
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 3:
+//         console.log("apri");
+//         break;
+
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+const Month = "march"
+switch (Month) {
+    case "january":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
