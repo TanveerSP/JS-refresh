4bac12a9195032a5f4d9500f7211a18b104a505f
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-10-16 15:47:25 +0530

    if and if else and else

---
+++ 04_Control/one.js
@@ -0,0 +1,68 @@
+// if 
+
+// if (codition) { // <= true hai to under ka code excute honga, ager flase hai to retrun hoga
+// }
+
+// const temprature = 41;
+// if (temprature === 40) {
+//     console.log("less then 50 ")
+// }
+// else {
+//     console.log("temprature is greater then 50");
+// }
+
+
+// compare oprator  
+// < less then --> less then me first value chote hoti hai or second value bade eg. 3<5 true, 5<1 false.
+// > greter then --> greter then me first value big our second me small hoti hai eg. 5>3 true, 3>5 false.
+// <= less then equal
+// >= greter then equal
+// == equal
+// != not equal
+// === tripal equal
+
+// const score = 200
+// if(score > 100 ) {
+//   const power = 'Fly'
+//   console.log(`User power: ${power}`);
+// }
+//  console.log(`User power: ${power}`); <--- give error 
+
+const balance = 1000
+
+// if(balance > 500) console.log("test"), console.log("test2"); // ==> avoid like write this code
+
+// if (balance < 500) {
+//     console.log("Less then");
+// } else if (balance < 750) {
+//     console.log("Less then");
+// } else if(balance < 900){
+//     console.log("Less then");
+// } else {
+//     console.log("less then 1200")
+// }
+
+const UserLoggedIn = true;
+const debitCard = true;
+
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true
+
+// if (UserLoggedIn && debitCard && 2==3) {
+//     console.log("Allow to buy course ");
+// }
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in")
+}
+
+
+
+
+
+
+
+
+
+
+
