d03edb849ecab06c8a6d8ebfda845e240acea1fb
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-10-16 16:05:49 +0530

    note

---
+++ 04_Control/truthy.js
@@ -0,0 +1,10 @@
+// const userEmail = "t@nveer.ai"
+// const userEmail = ""
+// const userEmail = []
+const userEmail = 1
+
+if(userEmail) {
+    console.log("Got user email")
+} else{
+    console.log("Don't have user email")
+}
